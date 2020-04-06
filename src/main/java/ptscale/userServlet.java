d128b245936b0a839cbd861e02ad7c6309136448
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-04-06 19:01:54 +0200

    m14

--- src/main/java/ptscale/userServlet.java
+++ src/main/java/ptscale/userServlet.java
@@ -6,9 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class userServlet
- */
+
 public class userServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	usersMongo input = new usersMongo();
