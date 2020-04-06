cb315156ea2e0b40a9292a046bbd19df2fa5c7f0
Zdzichu <Zdzichu@LAPTOP-ATQM63LV.mshome.net>
2020-04-06 11:01:49 +0200

    m4

--- src/main/java/ptscale/usersServlet.java
+++ src/main/java/ptscale/usersServlet.java
@@ -63,7 +63,7 @@ public class usersServlet extends HttpServlet {
 						  
 							request.setAttribute("usersAllForward", usersAll);
 							request.setAttribute("usersstring", sizestring);
-						 	request.getRequestDispatcher("user.jsp").forward(request, response);   		 		
+						 	//request.getRequestDispatcher("user.jsp").forward(request, response);   		 		
 						   // RequestDispatcher req = request.getRequestDispatcher("user.jsp");
 							//req.include(request, response);		
                            response.sendRedirect("user.jsp");  
